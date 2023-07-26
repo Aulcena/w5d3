5d84844fac1753a8ba1e3a75e7a83274f74c209d
tanziti <tanzitian3@gmail.com>
2023-07-26 14:07:03 -0400

    creatingclasses

--- practice-for-ch-sql-aa-questions-long-practice-main/QuestionsDB.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/QuestionsDB.rb
@@ -10,3 +10,17 @@ class QuestionsDatabase < SQLite3::Database
     self.results_as_hash = true
   end
 end
+
+class Users
+
+  def self.find_by_name(fanme, lname)
+
+  end
+
+  def initialize(options)
+    id = options['id']
+    fname = options['fname']
+    lname = options['lname']
+
+  end
+end 
