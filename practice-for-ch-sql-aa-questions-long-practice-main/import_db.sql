fc6f8a1f3f8efc7a0d95d76e8b43aa16e9229d37
AJU <aulcena@gmail.com>
2023-07-26 11:14:19 -0400

    add replies table

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -2,8 +2,6 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL,
   lname TEXT NOT NULL,
-
-
 );
 
 CREATE TABLE questions (
@@ -12,5 +10,28 @@ CREATE TABLE questions (
   body TEXT NOT NULL,
   user_id INTEGER,
   FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  questions_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (questions_id) REFERENCES questions(id)
+);
+
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  body TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  replies_id INTEGER,
+  questions_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (replies_id) REFERENCES replies(id),
+  FOREIGN KEY (questions_id) REFERENCES questions(id)
 );
 
