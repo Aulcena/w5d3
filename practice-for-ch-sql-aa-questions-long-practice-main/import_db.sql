de4eb23e22c7b039351a4e341e8e14bd7ed6bd3b
tanziti <tanzitian3@gmail.com>
2023-07-26 11:39:42 -0400

    readyfor1sttest

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL,
@@ -34,4 +36,33 @@ CREATE TABLE replies (
   FOREIGN KEY (replies_id) REFERENCES replies(id),
   FOREIGN KEY (questions_id) REFERENCES questions(id)
 );
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  likes INTEGER,
+  questions_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (questions_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+INSERT INTO 
+  users (fname, lname)
+VALUES
+  ('AJ', 'Ulcena'),
+  ('Zi', 'Tan');
+
+INSERT INTO
+  questions (title, body)  
+VALUES
+  ('Lunch?', 'Anyone want to get lunch?'),
+  ('study?', 'Anyone want to study?');
+
+INSERT INTO
+  replies (body, user_id, questions_id)
+VALUES
+  ('Yes, let"s grab lunch', (SELECT id FROM users WHERE fname LIKE 'AJ'),(SELECT id FROM questions WHERE title like 'Lunch?'));
+
+
 
