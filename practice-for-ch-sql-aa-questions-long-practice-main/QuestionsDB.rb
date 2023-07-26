9b39b144377e36b9e477124c192b31c9d0e75d0c
AJU <aulcena@gmail.com>
2023-07-26 15:10:33 -0400

    tested User class methods

--- practice-for-ch-sql-aa-questions-long-practice-main/QuestionsDB.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/QuestionsDB.rb
@@ -3,7 +3,7 @@ require 'singleton'
 
 
 class QuestionsDatabase < SQLite3::Database 
-  include 'singleton'
+  include Singleton
   def initialize
     super('questions.db')
     self.type_translation = true
@@ -11,16 +11,28 @@ class QuestionsDatabase < SQLite3::Database
   end
 end
 
-class Users
+class User
 
-  def self.find_by_name(fanme, lname)
+  attr_accessor :id, :fname, :lname
+
+  def self.find_by_name(fname, lname)
+    name =  QuestionsDatabase.instance.execute("SELECT * FROM users WHERE users.fname = fname AND users.lname = lname")
+    user_name = User.new(name.first)
+    
 
   end
 
-  def initialize(options)
-    id = options['id']
-    fname = options['fname']
-    lname = options['lname']
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute("SELECT * FROM users WHERE users.id = id")
+    # data.map {|datum| Users.new(datum)}
+    # data => [ {id: 3, fname: 'kin', lname: 'tse' } ]
+    user = User.new(data.first)
+  end
+
+  def initialize(options) #hash
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
 
   end
 end 
